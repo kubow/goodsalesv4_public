44ee8943d32ff12ead59e572a5d9262a53ff9dde
kubow <kube.kubow@gmail.com>
2025-10-01 23:38:34 +0200

    base url add to router

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ import Benefits from "./pages/OtherPage/Benefits";
 export default function App() {
   return (
     <>
-      <Router>
+      <Router basename={import.meta.env.BASE_URL}>
         <ScrollToTop />
         <Routes>
           {/* Dashboard Layout */}
